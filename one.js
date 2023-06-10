cacda4713841841b3e87baa3dec5baa8045b6729
sneha12199 <khoklesneha5@gmail.com>
2023-06-10 14:48:55 +0530

    okie

---
+++ one.js
@@ -0,0 +1,23 @@
+var a=100
+var b=200.45
+var c="300"
+var d="s"
+var e="fourty"
+var f=true
+var g=false
+var h=""
+console.log("plus operator acting two roles")
+console.log(a*b)
+console.log(a*c)
+console.log(a*d)
+console.log(a*e)
+console.log(a*f)
+console.log(a*g)
+console.log(a*h)
+console.log(a+b)
+console.log(a+c)
+console.log(a+d)
+console.log(a+e)
+console.log(a+f)
+console.log(a+g)
+console.log(a+h)
