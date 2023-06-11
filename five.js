4017f8c8fa2850a336a4167c229a5a74b36f0493
sneha12199 <khoklesneha5@gmail.com>
2023-06-11 12:28:03 +0530

    ok

---
+++ five.js
@@ -0,0 +1,16 @@
+var price=80000;
+var rating=4.9;
+var product_name="iphone promax"
+var brand_name='Apple'
+var size="M"
+console.log(typeof price)//number
+console.log(typeof rating)//number
+console.log(typeof product_name)//string
+console.log(typeof brand_name)//string
+console.log(typeof size)//string
+
+console.log( price)
+console.log( rating)
+console.log( product_name)
+console.log( brand_name)
+console.log( size)
