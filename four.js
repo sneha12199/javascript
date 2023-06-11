6518b87b81d3b7461e5b1fdc35251b4e16b955f2
sneha12199 <khoklesneha5@gmail.com>
2023-06-11 12:13:13 +0530

    ok

---
+++ four.js
@@ -0,0 +1,10 @@
+let enames=["sneha,vamsi,shiva,krishna,snevam"]
+let emp = {
+    id: 101,
+    name :"sneha",
+    salary :890000,
+}
+    console.log(enames)
+    console.log(emp)
+    console.log(typeof enames)
+    console.log(typeof emp)
