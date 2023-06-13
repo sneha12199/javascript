6d6647bf1f596ffcf6d789f5288b9d3d6792ceb3
sneha12199 <khoklesneha5@gmail.com>
2023-06-13 12:56:00 +0530

    test

---
+++ Conditionalst/1.js
@@ -0,0 +1,15 @@
+// //let num=3458
+// if(num % 2==0){
+//     console.log("even number")
+// }
+// else{
+//     console.log("odd number")
+// }
+
+let num=1359
+if (num % 2==0){
+    console.log("even no.")
+}
+else{
+    console.log("odd no.")
+}
