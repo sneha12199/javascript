64573354e76cc3fa09d1ed1089121e0f6d4eb252
sneha12199 <khoklesneha5@gmail.com>
2023-06-14 15:50:49 +0530

    test

---
+++ javascriptconditionalst/greatestthreeno.js
@@ -0,0 +1,13 @@
+let a=50
+let b=86
+let c=10
+if(a>b && a>c){
+    console.log("a is greatest no.")
+}
+else if(b>c)
+{
+    console.log("b is greatest no.")
+}
+else{
+    console.log("c is greatest no.")
+}
