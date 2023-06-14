8f70570a83114c0cd847069acf8bf96a9535ebab
sneha12199 <khoklesneha5@gmail.com>
2023-06-14 15:41:18 +0530

    test

--- javascriptconditionalst/greatestno.js
+++ javascriptconditionalst/greatestno.js
@@ -1,9 +1,9 @@
 let a=50
 let b=86
-if(b>a==0){
+if(a>b){
     console.log("a is greatest.")
 }
 else
-(a<b==0);{
+{
     console.log("b is greatest.")
 }
---
+++ javascriptconditionalst/leastno.js
@@ -0,0 +1,9 @@
+let a=50
+let b=86
+if(a<b){
+    console.log("a is least no.")
+}
+else
+{
+    console.log("b is least no.")
+}
