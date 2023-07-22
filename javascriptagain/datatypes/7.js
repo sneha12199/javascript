863392ba2b97ef146fb963f85a97ff746dfa88cb
sneha12199 <khoklesneha5@gmail.com>
2023-07-22 18:37:48 +0530

    ok

---
+++ javascriptagain/datatypes/7.js
@@ -0,0 +1,5 @@
+let array=[10,20,30,40,50,60,70,80]
+console.log(array)
+let new_length=array.push(90,100)
+console.log(array)
+console.log(new_length)
---
+++ javascriptagain/datatypes/8.js
@@ -0,0 +1,5 @@
+//pop method-remove last element
+let array=[10,20,30,40,50]
+let removed_element=array.pop()
+console.log(array)
+console.log(removed_element)
