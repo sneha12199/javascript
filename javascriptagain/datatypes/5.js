c8b421c585e935ec0a0835a2c876f7d35b8b2a77
sneha12199 <khoklesneha5@gmail.com>
2023-07-22 18:26:04 +0530

    yes

---
+++ javascriptagain/datatypes/5.js
@@ -0,0 +1,17 @@
+
+
+var price=500;
+var rating=4.9;
+var brand_name='bata'
+var product_name="white gown"
+var size="M"
+console.log(typeof price)
+console.log(typeof rating)
+console.log(typeof brand_name)
+console.log(product_name)
+console.log(typeof size)
+console.log(price)
+console.log(rating)
+console.log(brand_name)
+console.log(product_name)
+console.log(size)
---
+++ javascriptagain/datatypes/6.js
@@ -0,0 +1,11 @@
+
+
+
+
+//product
+var price=48900
+var rating=4.9
+console.log(price)
+console.log(rating)
+console.log(typeof price)
+console.log(typeof rating)
